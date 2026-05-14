01c1e63dbccd6dc277881bbf17b0153569bf5eb0
OGLingling <OGLingling@users.noreply.github.com>
2026-05-14 01:24:10 +0000

    deploy: ebec26fe9693801f2e5a02c6c503227e2ee77dfa

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "56532960" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3806000384" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
